08f9d36ec532b1db4cb118cf4ae0a9dcea8b81a4
Manon <manonlrx48@gmail.com>
2026-02-03 18:38:12 +0100

    feat: ajouter composant OptimizedImage avec support WebP

---
+++ src/components/OptimizedImage.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const OptimizedImage = ({
+  src,
+  webp,
+  alt = "",
+  width,
+  height,
+  loading = "lazy",
+  className = "",
+  onClick,
+  style,
+  ...props
+}) => {
+  if (!webp) {
+    return (
+      <img
+        src={src}
+        alt={alt}
+        width={width}
+        height={height}
+        loading={loading}
+        className={className}
+        onClick={onClick}
+        style={style}
+        {...props}
+      />
+    );
+  }
+
+  return (
+    <picture>
+      <source srcSet={webp} type="image/webp" />
+      <img
+        src={src}
+        alt={alt}
+        width={width}
+        height={height}
+        loading={loading}
+        className={className}
+        onClick={onClick}
+        style={style}
+        {...props}
+      />
+    </picture>
+  );
+};
+
+export default OptimizedImage;
